846403d277ae00b02263bf01b9f424e036b40330
przemek899 <przemek.899@wp.pl>
2023-11-25 23:28:38 +0100

    gui changes

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ from src.Equation_solver import Solver
 import easygui
 import os
 import sys
+
 """"""""""""""""""""""""""""""""""""""
 path1 = "./data/txt/Test1.txt"
 path2 = "./data/txt/Test2.txt"
 path3 = "./data/txt/Test3.txt"
-f_1 = lambda x: 5*(x**2) + 3*x + 6
-f_2 = lambda x,y: 5*(x**2) * (y**2) + 3*x*y + 6
+f_1 = lambda x: 5 * (x ** 2) + 3 * x + 6
+f_2 = lambda x, y: 5 * (x ** 2) * (y ** 2) + 3 * x * y + 6
 """"""""""""""""""""""""""""""""""""""
 # test_write(path=path1)
 # test_write(path=path2)
@@ -19,49 +20,52 @@ f_2 = lambda x,y: 5*(x**2) * (y**2) + 3*x*y + 6
 # test_macierz_HBC(points=2,grid=Grid(path2),global_data=Global_Data(path2))
 # test_vectorP(points=2,grid=Grid(path2),global_data=Global_Data(path2))
 """"""""""""""""""""""""""""""""""""""""" ""MAIN"""
+
+
 def get_txt(grid_list):
-    msg = "Wybierz plik:"
-    title = "Wybór pliku"
-    txt = easygui.choicebox(msg, title, grid_list)
-    if txt is None:
-        sys.exit()
-    return txt
+    while True:
+        msg = "Wybierz plik:"
+        title = "Plik z siatkami"
+        txt = easygui.choicebox(msg, title, grid_list)
+        if txt is None:
+            sys.exit()
+        return txt
+
+
 def get_points():
     msg = "Wybierz schemat punktowy:"
-    title = "Wybór schematu punktowego"
+    title = "Schemat całkowania"
     lista_schematow = ["2", "3", "4"]
     points = easygui.choicebox(msg, title, lista_schematow)
     if points is None:
         sys.exit()
     return int(points)
+
+
 def main():
-    while True:
-        grid_list = os.listdir("./data/txt")
-        txt = get_txt(grid_list)
-        points = get_points()
+    grid_list = os.listdir("./data/txt")
+    txt = get_txt(grid_list)
+    points = get_points()
 
-        path = os.getcwd() + f"/data/txt/{txt}"
-        global_data = Global_Data(path)                             # Dane globalne
-        grid = Grid(path)                                           # Utworz siatkę
+    path = os.getcwd() + f"/data/txt/{txt}"
+    global_data = Global_Data(path)  # Dane globalne
+    grid = Grid(path)  # Utworz siatkę
 
-        matrix_H = Matrix_H(points,path)                            # Uzyskaj macierze sztywności H dla wszystkich elementów
-        matrix_HBC = Matrix_HBC(points,grid,global_data)            # Uzyskaj macierze HBC dla wszystkich elementów
-        matrix_vecP = VectorP(points,grid,global_data)
+    matrix_H = Matrix_H(points, path)  # Uzyskaj macierze sztywności H dla wszystkich elementów
+    matrix_HBC = Matrix_HBC(points, grid, global_data)  # Uzyskaj macierze HBC dla wszystkich elementów
+    matrix_vecP = VectorP(points, grid, global_data)
 
+    for i, element in enumerate(grid.elements):
+        element.matrix_H = matrix_H.get_H_matrices()[i]  # Dodaj macierz H do elementów
+        element.matrix_HBC = matrix_HBC.get_HBC_matrices()[i]  # Dodaj macierz HBC do elementów
+        element.vectorP = matrix_vecP.get_vectorP_matrices()[i]  # Dodaj VectorP do elementów
 
-        for i,element in enumerate(grid.elements):
-            element.matrix_H = matrix_H.get_H_matrices()[i]             # Dodaj macierz H do elementów
-            element.matrix_HBC = matrix_HBC.get_HBC_matrices()[i]       # Dodaj macierz HBC do elementów
-            element.vectorP = matrix_vecP.get_vectorP_matrices()[i]     # Dodaj VectorP do elementów
+    solver = Solver(grid, global_data)  # Klasa do rozwiązywania układu równań
+    solver.test_H_global()              # Test złożenia macierzy H_global
+    solver.test_P_global()              # Test złożenia wektora  P_global
 
-        solver = Solver(grid,global_data)
-        solver.test_H_global()
-        solver.test_P_global()
-
-        solver.solve()
-
+    solver.solve()                      # Rozwiązanie układu równań
 
 
 if __name__ == "__main__":
     main()
-
